6403acfedeac15ed97f8a4b33f48eee0c3c4c035
ashika1107 <govindarasuashika@gmail.com>
2025-08-15 02:54:18 +0530

    Update and rename placeholder to init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,23 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+DROP DATABASE IF EXISTS DataWarehouse_bronze;
+DROP DATABASE IF EXISTS DataWarehouse_silver;
+DROP DATABASE IF EXISTS DataWarehouse_gold;
+
+CREATE DATABASE DataWarehouse_bronze;
+CREATE DATABASE DataWarehouse_silver;
+CREATE DATABASE DataWarehouse_gold;
+
