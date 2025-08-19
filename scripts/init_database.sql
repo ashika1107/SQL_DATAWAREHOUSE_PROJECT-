8b65d713d4fcec18060fed0da4113e481e1bc3b9
ashika1107 <govindarasuashika@gmail.com>
2025-08-19 11:56:01 +0530

    init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -13,11 +13,30 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
-DROP DATABASE IF EXISTS DataWarehouse_bronze;
-DROP DATABASE IF EXISTS DataWarehouse_silver;
-DROP DATABASE IF EXISTS DataWarehouse_gold;
+USE master;
+GO
 
-CREATE DATABASE DataWarehouse_bronze;
-CREATE DATABASE DataWarehouse_silver;
-CREATE DATABASE DataWarehouse_gold;
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
